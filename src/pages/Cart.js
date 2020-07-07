308a6cda1a9678bf4ad09a5a88d743253564e9dd
Luis Zavala <cionagi@hotmail.cl>
2020-07-07 01:08:56 -0400

    ADD page Cart

---
+++ src/pages/Cart.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import Layout from '../layout/Layout';
+import ProductDetailCart from '../components/cart/ProductListCart';
+import TotalCart from '../components/cart/TotalCart';
+import { cartState } from '../store/reducers/cart';
+
+const CartPage = ({}) => {
+  const cart = useSelector(cartState);
+  return (
+    <Layout>
+      <h1>Detalle de compra</h1>
+      {cart.total > 0 ? (
+        <Row className="align-items-start">
+          <Col xs={12} md={8}>
+            <ProductDetailCart cart={cart} />
+          </Col>
+          <Col xs={12} md={4}>
+            <Row>
+              <Col xs={12}>{cart.total > 0 && <TotalCart cart={cart} />}</Col>
+            </Row>
+          </Col>
+        </Row>
+      ) : (
+        <div className="mt-5 p-3 mb-2 bg-warning text-dark text-center">
+          <h5 className="mt-2 text-secondary">Su carrito esta vacio :(</h5>
+        </div>
+      )}
+    </Layout>
+  );
+};
+
+export default CartPage;

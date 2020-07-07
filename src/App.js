52436e21bc3b12e16b102835de0fad4f8c581357
Luis Zavala <cionagi@hotmail.cl>
2020-07-07 01:04:30 -0400

    UPDATE main page

--- src/App.js
+++ src/App.js
@@ -1,57 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CartPage from './pages/Cart';
+import CatalogPage from './pages/Catalog';
+import CheckoutSuccess from './pages/CheckoutSuccess';
+import ErrorPage from './pages/Error';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <CatalogPage />
+          </Route>
+          <Route path="/catalog">
+            <CatalogPage />
+          </Route>
+          <Route path="/cart">
+            <CartPage />
+          </Route>
+          <Route path="*">
+            <ErrorPage />
+          </Route>
+          <Route path="/success">
+            <CheckoutSuccess />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;

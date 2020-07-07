b578e4fe9c8564277b27f74cbb261729d055cb81
Luis Zavala <cionagi@hotmail.cl>
2020-07-07 01:09:13 -0400

    ADD page Error

---
+++ src/pages/Error.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Layout from '../layout/Layout';
+
+const ErrorPage = ({}) => {
+  return (
+    <Layout>
+      <Alert variant="danger">
+        <Alert.Heading>Error! </Alert.Heading>
+        <p>Esto no le gusta a Mario</p>
+        <hr />
+        <Link to="/">Volver al Home</Link>
+      </Alert>
+    </Layout>
+  );
+};
+
+export default ErrorPage;

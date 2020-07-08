04f2c1fc32fd6cf6e51dc26dbe4c3e2f1974b868
Luis Zavala <cionagi@hotmail.cl>
2020-07-07 21:50:44 -0400

    ADD filters products

---
+++ src/components/Filters.js
@@ -0,0 +1,35 @@
+// Dependency
+import React, { useEffect } from 'react';
+import { Accordion, Button, Card } from 'react-bootstrap';
+
+const Filters = ({ filterList = [], filterAction, filterSelected }) => {
+  const renderFilters = () => {
+    return filterList.map((filter) => {
+      return (
+        <Button
+          variant="outline-secondary"
+          className="m-1"
+          active={filter === filterSelected}
+          onClick={() => filterAction(filter)}
+        >
+          {filter}
+        </Button>
+      );
+    });
+  };
+
+  return (
+    <Accordion defaultActiveKey="0" className="mb-4">
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey="0">
+          Filtrar por serie
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>{renderFilters()}</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  );
+};
+
+export default Filters;
--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,6 +1,6 @@
 // Dependency
-import React, { useEffect } from 'react';
-import { Button, Col, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 // Store
 import { useDispatch, useSelector } from 'react-redux';
@@ -11,12 +11,16 @@ import Layout from '../layout/Layout';
 import Cart from '../components/cart/cart';
 import ProductList from '../components/product/productList';
 import SpinnerCustom from '../components/SpinnerCustom';
+import Filters from '../components/Filters';
 
 const CatalogPage = () => {
   const dispatch = useDispatch();
   const list = useSelector(productListState);
   const isFetching = useSelector(isFetchingState);
 
+  const [filters, setFilters] = useState([]);
+  const [productList, setProductList] = useState([]);
+  const [filterSelected, setFilterSelected] = useState();
   useEffect(() => {
     if (!list.length) {
       dispatch(addProducts());
@@ -24,12 +28,37 @@ const CatalogPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    const filterDump = list.reduce((a, r) => {
+      return a.includes(r.gameSeries) ? [...a] : [...a, r.gameSeries];
+    }, []);
+    setFilters([...filterDump]);
+    setProductList(list);
+  }, [list]);
+
+  const filterProductsBySerie = (gameSeries) => {
+    const listDump = list.filter((item) => {
+      return item.gameSeries === gameSeries;
+    });
+    setFilterSelected(gameSeries);
+    setProductList(listDump);
+  };
+
   return (
     <Layout>
       <h1>Amiibo Shop</h1>
+      <Row>
+        <Col xs={12}>
+          <Filters
+            filterList={filters}
+            filterAction={filterProductsBySerie}
+            filterSelected={filterSelected}
+          />
+        </Col>
+      </Row>
       <Row className="align-items-start">
         <Col xs={12} md={8}>
-          {isFetching ? <SpinnerCustom /> : <ProductList productList={list} />}
+          {isFetching ? <SpinnerCustom /> : <ProductList productList={productList} />}
         </Col>
         <Col xs={12} md={4}>
           <Row>
